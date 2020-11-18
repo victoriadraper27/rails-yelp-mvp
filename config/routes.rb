e0413a429e368385ad4d8cf7c155a8b527b68854
Victoria Draper <victoria.draper27@gmail.com>
2020-11-18 12:15:24 +0900

    FEAT: Routes Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'restaurants/index'
-  get 'restaurants/show'
-  get 'restaurants/new'
-  get 'restaurants/create'
-  get 'restaurants/edit'
-  get 'restaurants/update'
-  get 'restaurants/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :restaurants
+  resources :restaurants, only: [:index, :show, :new, :create]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+5.times do
+  attributes = {
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+    category: Restaurant::CATEGORIES.sample
+  }
+  restaurant = Restaurant.create!(attributes)
+
+  puts "Created #{restaurant.name}"
+end
+
+puts "Finished! Created #{Restaurant.count} restaurants"
