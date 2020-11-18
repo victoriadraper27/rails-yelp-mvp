caa57e63cc441d318c69b899ecc283fde1e8da8a
Victoria Draper <victoria.draper27@gmail.com>
2020-11-18 13:05:52 +0900

    FEAT Restaurant Index Show New Views

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,22 +1,33 @@
 class RestaurantsController < ApplicationController
+before_action :set_restaurant, only: [:show]
+
   def index
+    @restaurants = Restaurant.all
   end
 
-  def show
-  end
+  def show; end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant), notice: 'Restaurant was successfully created.'
+    else
+      render :new
+    end
   end
 
-  def edit
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 
-  def update
-  end
-
-  def destroy
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :category, :address, :phone_number)
   end
 end
---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,27 @@
+class ReviewsController < ApplicationControllerdef
+  before_action :set_restaurant
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+
+    if @review.save
+      redirect_to restaurant_path(@restaurant), notice: 'Review was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
+end
