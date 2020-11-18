b5f950bfda4a9ce430b8ed33ef6016352498e47c
Victoria Draper <victoria.draper27@gmail.com>
2020-11-18 11:40:40 +0900

    FEAT: Updated Models Validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,6 @@
 class Restaurant < ApplicationRecord
-  has_many :reviews
+  CATEGORIES = [ 'chinese', 'italian', 'japanese', 'french', 'belgian' ]
+  has_many :reviews, dependent: :destroy
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: { in: TYPES }
 end
