77eee3aa2b94325dca34dc855dd3a7f1c0ffdca1
Victoria Draper <victoria.draper27@gmail.com>
2020-11-18 11:29:11 +0900

    FEAT: Restaurant Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'restaurants/index'
+  get 'restaurants/show'
+  get 'restaurants/new'
+  get 'restaurants/create'
+  get 'restaurants/edit'
+  get 'restaurants/update'
+  get 'restaurants/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :restaurants
 end
